d6a52528f7186b6e1b35f0abfb668b9378c18fab
hxsan-k <hasan8812011@hotmail.co.uk>
2025-09-23 09:51:41 +0000

    Add level 1 solution and make levels/ directory

---
+++ levels/level1.sh
@@ -0,0 +1,11 @@
+#This script was written on a Killercoda environment, where ~ = /root
+
+#!/bin/bash
+
+mkdir ~/arena
+
+touch /root/arena/warrior.txt
+touch /root/arena/mage.txt
+touch /root/arena/archer.txt
+
+ls arena

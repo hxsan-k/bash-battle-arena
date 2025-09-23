f97c60ea68d2010e433d43395ae685f89f8a10ab
Hasan K <hasan8812011@hotmail.co.uk>
2025-09-23 15:59:56 +0100

    Add level instructions and 'else' statement for flow

--- levels/level5.sh
+++ levels/level5.sh
@@ -1,3 +1,7 @@
+# Creates "battlefield" directory and 3 text files inside
+# It then checks if knight.txt exists and moves to a new directory called "archive"
+# Finally, both directories contents are listed
+
 #!/bin/bash
 
 mkdir -p battlefield; cd battlefield
@@ -9,6 +13,8 @@ cd ..
 if [ -f battlefield/knight.txt ]; then
     mkdir -p archive
     mv battlefield/knight.txt archive/
+else
+    echo "knight.txt not found."
 fi
 
 echo "Battlefield contents: $(ls battlefield)"

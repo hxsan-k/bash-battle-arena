b3fd56fbc4f20a8cd5c04800a061e971b9af8068
Hasan K <hasan8812011@hotmail.co.uk>
2025-09-23 11:56:44 +0100

    Delete redundant comment

--- levels/level1.sh
+++ levels/level1.sh
@@ -1,5 +1,3 @@
-#This script was written on a Killercoda environment, where ~ = /root
-
 #Script creates a directory named Arena and then inside it, creates three files: 
 #warrior.txt, mage.txt, and archer.txt
 #It then lists the contents of the directory.

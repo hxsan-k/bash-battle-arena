a975a62dfcf4eaaf0728b9654203425e9418ecb8
Hasan Kamran <hasan8812011@hotmail.co.uk>
2025-10-17 10:35:08 +0100

    Enhance level6.sh to check file existence and input
    Update script to handle scenarios for missing filename and non-existent file.

--- levels/level6.sh
+++ levels/level6.sh
@@ -1,13 +1,19 @@
-# Script accepts filename and prints number of lines in file. If no filename is provided, message "no file provided" is displayed.
-
 #!/bin/bash
+# Script accepts filename and prints number of lines in file. 
+# Also handles 2 other scenarios where the file doesn't exist or no file name is provided by the user.
 
-filename () {
-if [ -n "$1" ]; then
-    echo "no. of lines in file: $(wc -l < "$1")"
+echo "please enter a file name"
+read FILE
+
+if [ -z "$FILE" ]
+then
+  echo "no file provided"
+
+elif [ -e "$FILE" ]
+then
+  echo "there are $(wc -l < "$FILE") lines in this file"
+
 else
-    echo "no file provided"
-fi
-}
+  echo "file does not exist"
 
-filename README.md
+fi
